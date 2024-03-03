f5a2733baf254ee23706238fe9637db2e0dd80c4
Manuel1598 <manuel.weilguni1@gmail.com>
2024-03-03 15:01:00 +0100

    Implementierung der asynchronen Ausführung mit RxJava anstelle von Threads. Hinzufügung erforderlicher Abhängigkeiten in der Gradle-Datei auf App-Ebene.

--- app/src/main/java/com/example/projekt1_weilguni_12139417/MainActivity.java
+++ app/src/main/java/com/example/projekt1_weilguni_12139417/MainActivity.java
@@ -6,14 +6,22 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.observers.DisposableObserver;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView responseTextView;
-
+    private DisposableObserver<String> disposableObserver;
+    private Disposable disposable;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -27,37 +35,49 @@ public class MainActivity extends AppCompatActivity {
         Button calculateButton = findViewById(R.id.calculateButton);
 
 
-
-
-        sendButton.setOnClickListener(v -> {
-            String matriculationNumber = matriculationEditText.getText().toString();
-
-            if (isValidMatriculationNumber(matriculationNumber)) {
-                new Thread(() -> {
-                    String response = new TCPClient().sendData(matriculationNumber);
-                    runOnUiThread(() -> displayResponse(response));
-                }).start();
+        disposableObserver = new DisposableObserver<String>() {
+            @Override
+            public void onNext(String response) {
+                displayResponse(response);
             }
-            else {
+
+            @Override
+            public void onError(Throwable e) {
                 displayErrorMessage();
             }
 
-        });
+            @Override
+            public void onComplete() {
+                Toast.makeText(MainActivity.this, "Serveranfrage abgeschlossen", Toast.LENGTH_SHORT).show();
+            }
+        };
 
+        sendButton.setOnClickListener(v -> {
+            String matriculationNumber = matriculationEditText.getText().toString();
+            if (isValidMatriculationNumber(matriculationNumber)) {
+                if (disposable != null && !disposable.isDisposed()) {
+                    disposable.dispose(); // Dispose previous subscription
+                }
+                disposable = getImageNetworkCall(matriculationNumber)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(disposableObserver);
+            } else {
+                displayErrorMessage();
+            }
+        });
 
 
         calculateButton.setOnClickListener(v -> {
             String matriculationNumber = matriculationEditText.getText().toString();
             if (isValidMatriculationNumber(matriculationNumber)) {
                 String sortedMatriculation = MatriculationSorter.sortMatriculationNumber(matriculationNumber);
-                runOnUiThread(() -> responseTextView.setText("Sortierte Matrikelnummer: " + sortedMatriculation));
-            }
-            else {
+                responseTextView.setText("Sortierte Matrikelnummer: " + sortedMatriculation);
+            } else {
                 displayErrorMessage();
             }
         });
     }
-
 
 
     @SuppressLint("SetTextI18n")
@@ -77,10 +97,21 @@ public class MainActivity extends AppCompatActivity {
         return matriculationNumber.length() == MATRICULATION_NUMBER_LENGTH;
     }
 
-
     @SuppressLint("SetTextI18n")
     private void displayErrorMessage() {
         responseTextView.setText("Matrikelnummer ist ungültig.");
+    }
+
+    private Observable<String> getImageNetworkCall(String matriculationNumber) {
+        return Observable.create(subscriber -> {
+            String response = new TCPClient().sendData(matriculationNumber);
+            if (response != null) {
+                subscriber.onNext(response);
+                subscriber.onComplete();
+            } else {
+                subscriber.onError(new Throwable("Keine Antwort vom Server erhalten."));
+            }
+        });
     }
 }
 
